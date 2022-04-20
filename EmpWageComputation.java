136b0e440d3e1df5f6cc0a6b960eb6ed6f369193
Mayoor <mayoordoiphode@gmail.com>
2022-04-21 00:28:52 +0530

    added: Calculating employee wages using switchcase

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -9,27 +9,28 @@ public class EmpWageComputation {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee wage computation program..!");
 //		checking whether employee is present 
-		int isPartTime = 1;
-		int isFullTime = 2;
-		int empRatePerHour = 20;
+		final int isPartTime = 1;
+		final int isFullTime = 2;
+		final int empRatePerHour = 20;
 		int empHrs = 0;
 		int empWage = 0;
 		
-		double empCheck = Math.floor(Math.random() * 10) % 3;
+		int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 // 		Check Present Employee is Part time or Full time
 		
-		if (empCheck == isPartTime) {
+		switch (empCheck) {
+		case isPartTime :
 			empHrs = 4;
 			System.out.println("\nEmployee is present, working part time..");
-		} 
-		if (empCheck == isFullTime) {
-			empHrs = 8;
+			break;
+		case isFullTime :
+			empHrs =8;
 			System.out.println("\nEmployee is present, working full time..");
-		}
-		if (empCheck==0) {
+			break;
+		default :
 			empHrs = 0;
-		System.out.println("\nEmployee is absent..");
-	}
+			System.out.println("\nEmployee is absent..");
+		}
 		
 		empWage = empHrs * empRatePerHour;
 		System.out.println("Employee Wages will be : " + empWage + " INR");
