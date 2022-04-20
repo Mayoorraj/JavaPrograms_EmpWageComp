a99a9d9647ef5610caaf1e2e6e82e0b889316004
Mayoor <mayoordoiphode@gmail.com>
2022-04-20 23:35:10 +0530

    added: Welcome messege prgram

---
+++ EmpWageComputation.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.assignment4.EmpWageComp;
+
+/**
+ * @author mayoor
+ *
+ */
+public class EmpWageComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee wage computation program..!");
+	}
+}
