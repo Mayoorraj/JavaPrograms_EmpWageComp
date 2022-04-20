0f41adbd8a6ef33fe13d1675c3d6ee168fd378ee
Mayoor <mayoordoiphode@gmail.com>
2022-04-21 02:11:19 +0530

    Refactored: Calculating total employee wages for a month for different companies

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,55 +5,62 @@ package com.bridgelabz.assignment4.EmpWageComp;
  *
  */
 public class EmpWageComputation {
-	//declared global variables
-		public static final int FULL_TIME = 1;
-		public static final int PART_TIME = 2;
-		public static final int EMP_WAGE_PER_HR = 20;
+	public static final int IS_FULL_TIME = 1; 
+	public static final int IS_PART_TIME = 2; 
+	
+	private final String company;
+	private final int empRatePerHr;
+	private final int numOfWorkingDays;
+	private final int maxHrsPerMonth;
+	private int totalEmpWage;
+	
+	public EmpWageComputation(String company, int empRatePerHour, int numOfWorkingDays, int maxHrsPerMonth) {
+		this.company = company;
+		this.empRatePerHr = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHrsPerMonth = maxHrsPerMonth;
+	}
+	
+	public void empWageCalculation() {
+		int totalEmpHour = 0;
+		int empHour = 0;
+		int totalWorkingDays = 0;
 		
-		//method: employee wage calculation	
-		public static void empWageComputation(String company, int empWagePerHr, int workingDayPerMonth, int maxHrsInMonth){
-				//variables initialization
-				 int empHrs = 0, totalEmpWage = 0, totalEmpHrs = 0, totalWorkingDays = 1 ;
-
-				  //check total emp hour not more than 100 and total days not more than 20
-			while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays <= workingDayPerMonth){
-				   double empCheck = Math.floor(Math.random()*3);
+		while(totalEmpHour <= maxHrsPerMonth && totalWorkingDays <= numOfWorkingDays) {
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch(empCheck) {
+			case IS_FULL_TIME:
+				empHour = 8;
+				break;
 				
-				        switch ((int)empCheck){
-						  case FULL_TIME:
-						       System.out.println("\nEmployee is present Full Time");
-						       empHrs = 8;
-						       break;
-						       
-						  case PART_TIME:
-						       System.out.println("\nEmployee is present Part Time");
-						       empHrs = 4;
-						       break;
-						  
-						  default:
-						       System.out.println("\nEmployee is Absent");
-						       empHrs = 0;	
-				        }
-				        
-				   //calculate total emp hours
-		                   totalEmpHrs += empHrs;
-				   System.out.println("Day : "+ totalWorkingDays +" Emp Hrs : "+empHrs);
-			       
-			        
-			      	   //day increment by 1 every iteration
-			           totalWorkingDays++ ;
-				}
-				 // calculate total emp wage
-				  totalEmpWage = totalEmpHrs * empWagePerHr;
-			          System.out.println("\nTotal Hours work done by Employee of "+company+" in a month is " + totalEmpHrs+ " Hrs"); 
-			          System.out.println("Employee of " +company +" is paid " +totalEmpWage+" INR\n---------------------------------" );
+			case IS_PART_TIME:
+				empHour = 4;
+				break;
+				
+			default:
+				empHour = 0;		
 			}
-		
-		public static void main(String[] args) {
-			System.out.println("Welcome to Employee Wage Computation Problem Part3!");
-			
-			//method call
-			EmpWageComputation.empWageComputation("WIPRO", 25, 5, 25);
-			EmpWageComputation.empWageComputation("COGNIZENT", 25, 15, 25);
-	   }     
+			totalEmpHour += empHour;
+		}
+		totalEmpWage = totalEmpHour * empRatePerHr;	
+	}
+	
+	@Override
+	public String toString() {
+		return "\nTotal Employee Wage for " + company + " is: " + totalEmpWage + " INR";
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation");
+		EmpWageComputation dMart = new EmpWageComputation("D-Mart", 24, 30, 150);
+		dMart.empWageCalculation();
+		System.out.println(dMart);
+		EmpWageComputation vijaySales = new EmpWageComputation("vijay Sales", 20, 30, 200);
+		vijaySales.empWageCalculation();
+		System.out.println(vijaySales);
+		EmpWageComputation relianceMart = new EmpWageComputation("Reliance Mart", 22, 30, 300);
+		relianceMart.empWageCalculation();
+		System.out.println(relianceMart);
+	}
 }
