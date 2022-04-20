3853c30fb8e1c9797cfaba9b2f26c29a754d28f3
Mayoor <mayoordoiphode@gmail.com>
2022-04-21 00:01:51 +0530

    added: Calculating employee wages

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -10,11 +10,23 @@ public class EmpWageComputation {
 		System.out.println("Welcome to Employee wage computation program..!");
 //		checking whether employee is present 
 		int isPresent = 1;
+		int EmpRatePerHour = 20;
+		int empHrs = 0;
+		int empWage = 0;
+
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		
+
 		if (empCheck == isPresent)
-			System.out.println("Employee is present..");
+			System.out.println("\nEmployee is present !");
 		else
-			System.out.println("Employee is absent..");
+			System.out.println("\nEmployee is absent..");
+
+//		calculation employee wage 	
+		if (empCheck == isPresent)
+			empHrs = 8;
+		else
+			empHrs = 0;
+		empWage = empHrs * EmpRatePerHour;
+		System.out.println("Employee wages will be : " + empWage + " INR");
 	}
 }
