ad5694e522954f2ae0e4e2c649c541c8c075f6f7
Mayoor <mayoordoiphode@gmail.com>
2022-04-22 15:42:16 +0530

    Added: Employee wages by multiple companies- interface

---
+++ EmpWageInterface.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.assignment4.EmpWageComp;
+
+public interface EmpWageInterface {
+	
+	public void addCompanyEmpWage(String company, int empRatePerHr, int numOfWorkingDays, int maxHrsPerMonth);
+	void empWage();
+}
