960a4b8aada200624fdadd5b725b06a151c046fd
Mayoor <mayoordoiphode@gmail.com>
2022-04-22 16:22:36 +0530

    Added: Employee wages by multiple companies- using arraylist

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.assignment4.EmpWageComp;
 
+import java.util.ArrayList;
+
 /**
  * @author mayoor
  *
@@ -8,28 +10,29 @@ public class EmpWageComputation implements EmpWageInterface {
 //	global variables
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
-// 	local variable
-	private int numOfCompanies = 0;
-//	calling constructor
-	private CompanyEmpWage[] companyEmpWageArray;
+	
+//	creating an array list
+	private ArrayList<CompanyEmpWage> companyEmpWageArrayList;
 
 //	creating an object of constructor
 	public EmpWageComputation() {
-		companyEmpWageArray = new CompanyEmpWage[10];
+		super();
+		companyEmpWageArrayList = new ArrayList<CompanyEmpWage>();
 	}
 
 //	defining method using array
 	public void addCompanyEmpWage(String company, int empRatePerHr, int numOfWorkingDays, int maxHrsPerMonth) {
-		companyEmpWageArray[numOfCompanies] = new CompanyEmpWage(company, empRatePerHr, numOfWorkingDays,
-				maxHrsPerMonth);
-		numOfCompanies++;
+		CompanyEmpWage companywage = new CompanyEmpWage(company, empRatePerHr, numOfWorkingDays,maxHrsPerMonth);
+		companyEmpWageArrayList.add(companywage);
 	}
 
 //	calculating wages for multiple company
-	void EmpWage() {
-		for (int i = 0; i < numOfCompanies; i++) {
-			companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
-			System.out.print(companyEmpWageArray[i]);
+	public void empWage() {
+		for (int i = 0; i < companyEmpWageArrayList.size(); i++) {
+			CompanyEmpWage companyEmpWage = companyEmpWageArrayList.get(i);
+			
+			int totalEmpWage = this.computeEmpWage(companyEmpWage);
+			System.out.println("Total Employee Wage for Company " + companyEmpWage.company + " is: " + totalEmpWage);
 		}
 	}
 
@@ -63,18 +66,13 @@ public class EmpWageComputation implements EmpWageInterface {
 //		Welcome message
 		System.out.println("Welcome to Employee Wage Computation program..!");
 
-//		Creating instances of array to compute multiple companies wage structure
+//		Creating instances of array list to compute multiple companies wage structure
 		EmpWageComputation companyEmpWageArray = new EmpWageComputation();
 		companyEmpWageArray.addCompanyEmpWage("D-Mart", 20, 30, 150);
 		companyEmpWageArray.addCompanyEmpWage("Big Basket", 25, 30, 200);
 		companyEmpWageArray.addCompanyEmpWage("Reliance Mart", 22, 30, 300);
 // 		calling method to compute wages for multiple companies
-		companyEmpWageArray.EmpWage();
+		companyEmpWageArray.empWage();
 
-	}
-
-	public void empWage() {
-		// TODO Auto-generated method stub
-		
 	}
 }
